7970675df35b6b71270b81840a489101ac52d3d4
Dan Octavian <danoctavian91@gmail.com>
2020-08-17 15:09:46 +0800

    add similar implementation to mcr-api

--- src/gas-price.js
+++ src/gas-price.js
@@ -1,10 +1,8 @@
 const fetch = require('node-fetch');
 const log = require('./log');
+const { to } = require('./utils');
 
-const ETHERCHAIN_GAS_API_URL = 'https://www.etherchain.org/api/gasPriceOracle';
-const ETHGASSTATION_GAS_API_URL = 'https://ethgasstation.info/json/ethgasAPI.json';
-
-const PRICE_LEVEL = {
+const SPEED = {
   SAFE_LOW: 'safeLow',
   STANDARD: 'standard',
   ABOVE_STANDARD: 'aboveStandard',
@@ -13,50 +11,68 @@ const PRICE_LEVEL = {
 };
 
 const GWEI_IN_WEI = 1e9;
+const ETHERCHAIN_URL = 'https://www.etherchain.org/api/gasPriceOracle';
+const ETHGASSTATION_URL = 'https://ethgasstation.info/json/ethgasAPI.json';
 
-async function getGasPrice (priceLevel) {
-  let prices;
-  try {
-    prices = await getETHGasStationPrices();
-  } catch (e) {
-    log.warn(`Failed to get gas price from ethgasstation: ${e.stack} Using fallback with etherchain..`);
-    prices = await getEtherchainPrices();
-  }
-  log.info(`Gas results: ${JSON.stringify(prices)}`);
-  const price = prices[priceLevel];
-  return price.toString();
-}
+/**
+ * Fetches gas prices from Etherchain with fallback to EthGasStation if the first fails
+ * @return {Promise<{standard: number, fast: number, fastest: number, safeLow: number}>}
+ */
+const fetchGasPrices = async () => {
 
-async function getEtherchainPrices () {
-  const data = await fetch(ETHERCHAIN_GAS_API_URL).then(res => res.json());
-  const prices = data;
-  for (const level of Object.keys(prices)) {
-    prices[level] = parseInt(prices[level]) * GWEI_IN_WEI;
-  }
-  prices[PRICE_LEVEL.ABOVE_STANDARD] = getAboveStandardPrice(prices);
-  return prices;
-}
+  const [ecPrice, ecError] = await to(fetch(ETHERCHAIN_URL).then(r => r.json()));
 
-async function getETHGasStationPrices () {
-  const data = await fetch(ETHGASSTATION_GAS_API_URL).then(res => res.json());
-  const prices = {};
-  prices[PRICE_LEVEL.SAFE_LOW] = data.safeLow / 10;
-  prices[PRICE_LEVEL.STANDARD] = data.average / 10;
-  prices[PRICE_LEVEL.FAST] = data.fast / 10;
-  prices[PRICE_LEVEL.FASTEST] = data.fastest / 10;
-  for (const level of Object.keys(prices)) {
-    prices[level] = prices[level] * GWEI_IN_WEI;
+  if (!ecError) {
+    return {
+      fastest: parseFloat(ecPrice.fastest),
+      fast: parseFloat(ecPrice.fast),
+      standard: parseFloat(ecPrice.standard),
+      safeLow: parseFloat(ecPrice.safeLow),
+    };
   }
 
-  prices[PRICE_LEVEL.ABOVE_STANDARD] = getAboveStandardPrice(prices);
-  return prices;
-}
+  log.error(`Failed to fetch Etherchain price data, using EthGasStation as a fallback: ${ecError.stack}`);
+
+  const [egsPrice, egsError] = await to(fetch(ETHGASSTATION_URL).then(r => r.json()));
+
+  if (egsError) {
+    log.error(`Failed to fetch EthGasStation: ${egsError.stack}`);
+    throw new Error('Gas price fetching failed');
+  }
+
+  return {
+    fastest: egsPrice.fastest / 10,
+    fast: egsPrice.fast / 10,
+    standard: egsPrice.average / 10,
+    safeLow: egsPrice.safeLow / 10,
+  };
+};
+
+/**
+ * Returns a recommended gas price considering the requested speed and upper limit
+ * @param speed {string}
+ * @param maxGasPriceInGwei {number}
+ * @return {Promise<number>}
+ */
+const getGasPrice = async (speed) => {
+  const prices = await fetchGasPrices();
+  prices[SPEED.ABOVE_STANDARD] = getAboveStandardPrice(prices);
+  log.info(JSON.stringify(prices));
+  const { [speed]: price } = prices;
+
+  if (!price) {
+    throw new Error(`No gas price found for '${speed}' speed`);
+  }
+
+  return Math.round(price * GWEI_IN_WEI);
+};
 
 function getAboveStandardPrice (prices) {
   return Math.floor((prices.fast + prices.standard)) / 2;
 }
 
 module.exports = {
+  fetchGasPrices,
   getGasPrice,
-  PRICE_LEVEL,
+  SPEED
 };
--- src/utils.js
+++ src/utils.js
@@ -12,8 +12,16 @@ function getEnv (key, fallback = false) {
   return value;
 }
 
+const to = promise => new Promise(resolve => {
+  promise
+    .then(r => resolve([r, null]))
+    .catch(e => resolve([null, e]));
+});
+
+
 module.exports = {
   hex,
   sleep,
   getEnv,
+  to
 };
